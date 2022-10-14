8542f57874d61910f64b8fe699580ac780ac0c99
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 21:07:39 +0530

    vowelorconsonant

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+public class vowelorconsonant
+{
+    static void Main(string[] args)
+    {
+        char ch;
+      
+        Console.WriteLine("check whether the input alphabet is a vowel or not:");
+        
+        Console.WriteLine("Input an Alphabet (A-Z || a-z) : ");
+        ch = Convert.ToChar(Console.ReadLine().ToLower());
+        int i = ch;
+        if (i >= 48 && i <= 57)
+        {
+            Console.WriteLine("You entered a number, Please enter an alpahbet.");
+        }
+        else
+        {
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is not a vowel");
+                    break;
+            }
+        }
+    }
+}
+
